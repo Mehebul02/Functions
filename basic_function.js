165f809691bf8e2fb90b931326ee47ceb727a90e
Mehebul02 <mehebul2123@gmail.com>
2024-02-04 00:27:16 +0600

    first commit

---
+++ basic_function.js
@@ -0,0 +1,7 @@
+const price = 454;
+function kalCOllage(){
+    console.log("Ami kal collage jbo nah")
+    console.log("Kno ki hoise je collage jabi nah")
+    console.log("Emnite ami jbo nah")
+}
+kalCOllage()
---
+++ peramiter.js
@@ -0,0 +1,8 @@
+function sum (number){
+    console.log("The parametar number of",number);
+    total =number + number;
+    console.log("total Number of:",total)
+}
+sum(44)
+sum(10)
+sum(30)
