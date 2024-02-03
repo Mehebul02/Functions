f9b50581d6865adfa19543eb2ffe9520275f73d6
Mehebul02 <mehebul2123@gmail.com>
2024-02-04 00:30:05 +0600

    sum added

--- peramiter.js
+++ peramiter.js
@@ -2,7 +2,11 @@ function sum (number){
     console.log("The parametar number of",number);
     total =number + number;
     console.log("total Number of:",total)
+    squre=number * number;
+    console.log("the squre number:",squre);
 }
 sum(44)
 sum(10)
 sum(30)
+sum(10)
+sum(10)
