67b0f4b1f2f97cec29e410ffe83124cfbaf96136
Mehebul02 <mehebul2123@gmail.com>
2024-02-04 20:22:42 +0600

    condional return

---
+++ condional_return.js
@@ -0,0 +1,19 @@
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// console.log(isEven(7));
+// console.log(isEven(20));
+
+function isAdd(number) {
+  if (number % 2 === 1) {
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+console.log(isAdd(37))
